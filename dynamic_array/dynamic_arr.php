93b33fe6fe20081c6b0b553cd11970f92ecb409d
mohammed <moa199528@gmail.com>
2024-02-10 09:19:49 -0800

    first commit

---
+++ dynamic_array/dynamic_arr.php
@@ -0,0 +1,69 @@
+<?php
+class Dynamic_array
+{
+    public $data = array();
+    public $size;
+
+    public function Dynamic_array()
+    {
+        $this->size = 0;
+        //تحديد حجم المصفوفه الي 1
+        $this->data = array_fill(0, 1, null);
+    }
+
+    //فحص كول المصفوفه ان كانت تحتمل اي عنصر او اي منتج اضافه
+    private function check_the_size_of_array($check_size)
+    {
+        $current_size = sizeof($this->data);
+        if ($current_size < $check_size) {
+            $new_size = $current_size * 2;
+            $this->data = array_pad($this->data, $new_size, null);
+            if ($current_size < $check_size) {
+                $new_size = $check_size;
+                $this->data = array_pad($this->data, $new_size, null);
+            }
+        }
+    }
+
+    //اضافه عنصر جديد للاراي 
+    public function add_item($new_item)
+    {
+        $this->check_the_size_of_array($this->size++);
+        $this->data[$this->size] = $new_item;
+    }
+
+    //حزف اي عنصر من الارراي
+    public function delete($item)
+    {
+        $new_arr = array();
+        $orginal_array = $this->data;
+        foreach ($orginal_array as $key => $data) {
+            # code...
+            $found = false;
+            if ($data != $item) {
+                $found = true;
+                $new_arr[] = $data;
+            }
+
+            if ($found) {
+                $this->data = $new_arr;
+
+            }
+        }
+    }
+
+    //select * data from array
+    public function get_data()
+    {
+        return $this->data;
+    }
+
+    //تفريغ المصفوفه من كافه البيانات
+    public function delete_all_data()
+    {
+        $this->data = [];
+        $this->size = 0;
+    }
+}
+
+?>
---
+++ dynamic_array/index.php
@@ -0,0 +1,34 @@
+<?php
+include_once('dynamic_arr.php');
+$D_arr = new Dynamic_array();
+$D_arr->add_item(1);
+$D_arr->add_item(2);
+$D_arr->add_item(3);
+$D_arr->add_item(4);
+$D_arr->add_item(7);
+
+$D_arr->delete_all_data();
+
+$D_arr->add_item(11);
+$D_arr->add_item(21);
+$D_arr->add_item(31);
+$D_arr->add_item(41);
+$D_arr->add_item(71);
+
+$D_arr->add_item(1);
+$D_arr->add_item(2);
+$D_arr->add_item(3);
+$D_arr->add_item(4);
+$D_arr->add_item(7);
+$D_arr->delete(1);
+$D_arr->delete(2);
+$D_arr->delete(3);
+$D_arr->delete(4);
+$D_arr->delete(7);
+
+echo "<pre>";
+print_r($D_arr->get_data());
+echo "</pre>";
+
+
+?>
