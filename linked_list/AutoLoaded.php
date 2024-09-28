bdd408a261d37887584dd568051c7fcb544688cd
mohammed <moa199528@gmail.com>
2024-09-28 10:09:16 -0700

    this is fourth commit

--- linked_list/AutoLoaded.php
+++ linked_list/AutoLoaded.php
@@ -3,8 +3,8 @@ spl_autoload_register(function ($ClassName) {
     if (file_exists('./' . $ClassName . '.php')) {
         include_once './' . $ClassName . '.php';
     } else {
-        if (file_exists('./ProblemSolving/' . $ClassName . '.php')) {
-            include_once './ProblemSolving/' . $ClassName . '.php';
+        if (file_exists('./ProblemSolving/' . $ClassName . '/' . $ClassName . '.php')) {
+            include_once './ProblemSolving/' . $ClassName . '/' . $ClassName . '.php';
         }
     }
 });
--- linked_list/ProblemSolving/PrintTheMiddleOfGivenLinkedList/AutoLoaded.php
+++ linked_list/ProblemSolving/PrintTheMiddleOfGivenLinkedList/AutoLoaded.php
@@ -1,7 +1,7 @@
 <?php
 spl_autoload_register(function ($ClassName) {
-    if (file_exists('../' . $ClassName . '.php')) {
-        include_once '../' . $ClassName . '.php';
+    if (file_exists('../../' . $ClassName . '.php')) {
+        include_once '../../' . $ClassName . '.php';
     }
     if (file_exists('./' . $ClassName . '.php')) {
         include_once './' . $ClassName . '.php';
